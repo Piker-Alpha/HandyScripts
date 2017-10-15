9e62c3b114e4e4688716107732fde69aa2364df1
Pike R. Alpha <bryan@iMac-Pro.local>
2017-10-15 02:46:42 +0200

    SeedEnrollment.plist

---
+++ High Sierra/DeveloperSeed.sh
@@ -0,0 +1,8 @@
+echo '<?xml version="1.0" encoding="UTF-8"?>'
+echo '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
+echo '<plist version="1.0">'
+echo '     <dict>'
+echo '             <key>SeedProgram</key>'
+echo '             <string>DeveloperSeed</string>'
+echo '     </dict>'
+echo '</plist>'
